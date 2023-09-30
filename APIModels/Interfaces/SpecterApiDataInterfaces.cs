f67cd622dee357fa682e5160dae6f856436a5f99
Mikhail Bhuta <mikhail@dirtcube.xyz>
2023-10-01 03:25:10 +0530

    [Doc] --added more comments to interfaces

--- APIModels/Interfaces/SpecterApiDataInterfaces.cs
+++ APIModels/Interfaces/SpecterApiDataInterfaces.cs
@@ -2,8 +2,16 @@ using System.Collections.Generic;
 
 namespace SpecterSDK.APIModels.Interfaces
 {
+    /// <summary>
+    /// Interface that marks a class as a valid data structure for API responses.
+    /// </summary>
     public interface ISpecterApiResponseData { }
 
+    /// <summary>
+    /// Interface for data structures that represent master data in the system.
+    /// Master data typically refers to objects configured on the Specter Dashboard
+    /// and has tags for categorization and meta information for custom details.
+    /// </summary>
     public interface ISpecterMasterData
     {
         public List<string> tags { get; set; }
--- APIModels/Interfaces/SpecterPropertyInterfaces.cs
+++ APIModels/Interfaces/SpecterPropertyInterfaces.cs
@@ -2,16 +2,30 @@ using System.Collections.Generic;
 
 namespace SpecterSDK.APIModels.Interfaces
 {
+    /// <summary>
+    /// Interface for request objects that require a project ID.
+    /// The SDK uses this interface to automatically set the project ID if it's missing in the request.
+    /// </summary>
     public interface IProjectConfigurable
     {
         public string projectId { get; set; }
     }
 
+    /// <summary>
+    /// Interface for request objects that can be customized with specific attributes.
+    /// Attributes can be any fields needed in the response.
+    /// <example>User attributes can be "firstName", "lastName", etc.</example>
+    /// </summary>
     public interface IAttributeConfigurable
     {
         public List<string> attributes { get; set; }
     }
 
+    /// <summary>
+    /// Interface for request objects that can be associated with multiple entities.
+    /// Entities can be any data structures or objects that the request's response relates to.
+    /// <example>An entity for a progression system is items - the item rewards for each level</example>
+    /// </summary>
     public interface IEntityConfigurable
     {
         public List<SPApiRequestEntity> entities { get; set; }
