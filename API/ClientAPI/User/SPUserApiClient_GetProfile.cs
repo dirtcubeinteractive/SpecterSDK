ac1b2808a04dca869c83cea01966c34768d8faab
Mikhail Bhuta <mikhail@dirtcube.xyz>
2023-10-03 14:32:09 +0530

    [User] --added sample non-asyn request to User get profile

--- API/ClientAPI/User/SPUserApiClient_GetProfile.cs
+++ API/ClientAPI/User/SPUserApiClient_GetProfile.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -44,6 +45,24 @@ namespace SpecterSDK.API.ClientAPI.User
 
             var result = await PostAsync<SPGetUserProfileResult, SPUserProfileResponseData>("/v1/client/user/get-profile", AuthType, request);
             return result;
+        }
+
+        public void GetProfile(SPGetUserProfileRequest request, Action<SPGetUserProfileResult> onComplete)
+        {
+            var defaultAttributes = new List<string>()
+            {
+                nameof(SPUserResponseBaseData.uuid),
+                nameof(SPUserResponseBaseData.id),
+                nameof(SPUserResponseBaseData.username),
+                nameof(SPUserResponseBaseData.hash)
+            };
+            
+            request.attributes ??= new List<string>();
+            request.attributes.AddRange(defaultAttributes);
+            request.attributes = request.attributes.Distinct().ToList();
+            
+            var task = PostAsync<SPGetUserProfileResult, SPUserProfileResponseData>("/v1/client/user/get-profile", AuthType, request);
+            task.GetAwaiter().OnCompleted(() => onComplete?.Invoke(task.Result));
         }
     }
 }
