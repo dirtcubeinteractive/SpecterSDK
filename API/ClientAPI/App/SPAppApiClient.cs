4c1fdb1b38593d1c41270f20b0ced9ee62d06f85
Mikhail Bhuta <mikhail@dirtcube.xyz>
2023-10-04 14:32:51 +0530

    [ClientApi] --add folders and init files for additional api clients

---
+++ API/ClientAPI/App/SPAppApiClient.cs
@@ -0,0 +1,9 @@
+using SpecterSDK.Shared;
+
+namespace SpecterSDK.API.ClientAPI.App
+{
+    public partial class SPAppApiClient : SpecterApiClientBase
+    {
+        public SPAppApiClient(SpecterRuntimeConfig config) : base(config) { }
+    }
+}
---
+++ API/ClientAPI/Progression/SPProgressionApiClient.cs
@@ -0,0 +1,9 @@
+using SpecterSDK.Shared;
+
+namespace SpecterSDK.API.ClientAPI.Progression
+{
+    public partial class SPProgressionApiClient : SpecterApiClientBase
+    {
+        public SPProgressionApiClient(SpecterRuntimeConfig config) : base(config) { }
+    }
+}
