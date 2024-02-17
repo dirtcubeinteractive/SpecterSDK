b39132794fa4a19d368073c35b950cb49a868025
Mikhail Bhuta <mikhail@dirtcube.xyz>
2024-02-17 14:30:12 +0530

    [Docs] --completed comment docs for matches api

--- API/ClientAPI/Matches/SPMatchesApiClient_CreateMatchSession.cs
+++ API/ClientAPI/Matches/SPMatchesApiClient_CreateMatchSession.cs
@@ -5,11 +5,19 @@ using SpecterSDK.APIModels.ClientModels;
 
 namespace SpecterSDK.API.ClientAPI.Matches
 {
+    /// <summary>
+    /// Represents the request to create a match session. Creating a match session only uses
+    /// the properties which it inherits from the base class <see cref="SPMatchSessionRequestBase"/>.
+    /// </summary>
     [System.Serializable, JsonObject(ItemNullValueHandling = NullValueHandling.Ignore)]
     public class SPCreateMatchSessionRequest : SPMatchSessionRequestBase { }
     
+    /// <summary>
+    /// Represents the result of creating a match session.
+    /// </summary>
     public class SPCreateMatchSessionResult : SpecterApiResultBase<SPMatchSessionResponseData>
     {
+        // The ID of the newly created match session.
         public string MatchSessionId;
 
         protected override void InitSpecterObjectsInternal()
@@ -20,6 +28,17 @@ namespace SpecterSDK.API.ClientAPI.Matches
 
     public partial class SPMatchesApiClient
     {
+        /// <summary>
+        /// Asynchronously creates a match session and returns the result.
+        /// <remarks>
+        /// This API call is only needed to be made if you wish to create a match session before players actually begin play
+        /// (e.g. before the start of a match in a multiplayer game).
+        /// In most cases, you would simply call the start match session endpoint when a match begins since
+        /// starting a match session internally creates a session if one doesn't exist.
+        /// </remarks>
+        /// </summary>
+        /// <param name="request"><see cref="SPCreateMatchSessionRequest"/> containing details about the match session to be created.</param>
+        /// <returns>A Task representing the asynchronous operation, with <see cref="SPCreateMatchSessionResult"/> as the result type.</returns>
         public async Task<SPCreateMatchSessionResult> CreateMatchSessionAsync(SPCreateMatchSessionRequest request)
         {
             var result = await PostAsync<SPCreateMatchSessionResult, SPMatchSessionResponseData>("/v1/client/matches/create-session", AuthType, request);
--- API/ClientAPI/Matches/SPMatchesApiClient_StartMatchSession.cs
+++ API/ClientAPI/Matches/SPMatchesApiClient_StartMatchSession.cs
@@ -6,14 +6,28 @@ using SpecterSDK.APIModels.ClientModels;
 
 namespace SpecterSDK.API.ClientAPI.Matches
 {
+    /// <summary>
+    /// Represents the request to start a match session.
+    /// </summary>
     [Serializable, JsonObject(ItemNullValueHandling = NullValueHandling.Ignore)]
     public class SPStartMatchSessionRequest : SPMatchSessionRequestBase
     {
+        /// <summary>
+        /// The ID of the match session to start.
+        /// <remarks>
+        /// Only applicable if create match session was called prior to starting.
+        /// Otherwise, only the other properties inherited from <see cref="SPMatchSessionRequestBase"/> are sufficient.
+        /// </remarks>
+        /// </summary>
         public string matchSessionId { get; set; }
     }
     
+    /// <summary>
+    /// Represents the result of starting a match session.
+    /// </summary>
     public class SPStartMatchSessionResult : SpecterApiResultBase<SPMatchSessionResponseData>
     {
+        // The ID of the match session that was started and/or created.
         public string MatchSessionId;
 
         protected override void InitSpecterObjectsInternal()
@@ -24,6 +38,11 @@ namespace SpecterSDK.API.ClientAPI.Matches
 
     public partial class SPMatchesApiClient
     {
+        /// <summary>
+        /// Asynchronously starts a match session and returns the result.
+        /// </summary>
+        /// <param name="request"><see cref="SPStartMatchSessionRequest"/> containing details about the match session to be started.</param>
+        /// <returns>A Task representing the asynchronous operation, with <see cref="SPStartMatchSessionResult"/> as the result type.</returns>
         public async Task<SPStartMatchSessionResult> StartMatchSessionAsync(SPStartMatchSessionRequest request)
         {
             var result = await PostAsync<SPStartMatchSessionResult, SPMatchSessionResponseData>("/v1/client/matches/start-session", AuthType, request);
