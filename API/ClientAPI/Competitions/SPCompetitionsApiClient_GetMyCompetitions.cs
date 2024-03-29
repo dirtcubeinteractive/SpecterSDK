771131c6cd2d113437d402bb1189904936040983
Mikhail Bhuta <mikhail@dirtcube.xyz>
2024-03-29 17:57:16 +0530

    [Competitions] --added get my competitions route

---
+++ API/ClientAPI/Competitions/SPCompetitionsApiClient_GetMyCompetitions.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using SpecterSDK.APIModels;
+using SpecterSDK.APIModels.ClientModels;
+using SpecterSDK.ObjectModels;
+
+namespace SpecterSDK.API.ClientAPI.Competitions
+{
+    [Serializable]
+    public class SPGetMyCompetitionsRequest : SPPaginatedApiRequest
+    {
+        public List<string> competitionIds { get; set; }
+    }
+
+    public class SPGetMyCompetitionsResult : SpecterApiResultBase<SPResponseDataList<SPEnteredCompetitionResponseData>>
+    {
+        public List<SpecterEnteredCompetition> MyCompetions;
+        
+        protected override void InitSpecterObjectsInternal()
+        {
+            MyCompetions = new List<SpecterEnteredCompetition>();
+            foreach (var comp in Response.data)
+            {
+                MyCompetions.Add(new SpecterEnteredCompetition(comp));
+            }
+        }
+    }
+    
+    public partial class SPCompetitionsApiClient
+    {
+        public async Task<SPGetMyCompetitionsResult> GetMyCompetitionsAsync(SPGetMyCompetitionsRequest request)
+        {
+            var result = await PostAsync<SPGetMyCompetitionsResult, SPResponseDataList<SPEnteredCompetitionResponseData>>("/v1/client/competitions/my-competitions", AuthType, request);
+            return result;
+        }
+    }
+}
