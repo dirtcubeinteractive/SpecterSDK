a59f802f934d333679dddb41355ee98484e54484
Mikhail Bhuta <mikhail@dirtcube.xyz>
2025-05-09 11:18:12 +0530

    [App] --Get AppInfo and GetGames responses added

---
+++ API/ClientAPI/v2/App/DTOs/Interfaces.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+using SpecterSDK.APIModels.ClientModels;
+
+namespace SpecterSDK.API.ClientAPI.v2.App.DTOs
+{
+    public interface ISpecterGameData : ISpecterResourceData
+    {
+        public string howTo { get; set; }
+        public List<string> screenshotUrls { get; set; }
+        public List<string> videoUrls { get; set; }
+        public List<SPAppPlatformData> platforms { get; set; }
+        public List<SPLocationData> locations { get; set; }
+        public List<SPGameGenreData> genres { get; set; }
+    }
+
+    public interface ISpecterItemPropData
+    {
+        public bool isConsumable { get; set; }
+        public bool isEquippable { get; set; }
+        public bool isTradable { get; set; }
+        public bool isStackable { get; set; }
+        public bool isLocked { get; set; }
+        public bool isDefaultLoadout { get; set; }
+        public bool equippedByDefault { get; set; }
+        public int stackCapacity { get; set; }
+        public int maxCollectionInstance { get; set; }
+        public int quantity { get; set; }
+        public int limitedQuantity { get; set; }
+        public int? consumeByUses { get; set; }
+        public int? consumeByTime { get; set; }
+        public string consumeByTimeFormat { get; set; }
+    }
+}
